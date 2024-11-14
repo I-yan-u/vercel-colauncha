542554abeb8da461719f112bf2a6df2c9062f7dd
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-11-14 11:36:23 +0100

    set up auth and protected route and added a dashboard section for making request

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -6,11 +6,12 @@ const Dashboard = () => {
   return (
     <div className="h-screen w-full">
       <div className="w-full mx-auto flex flex-col mt-28 gap-10 items-center justify-center">
-        <h1 className="font-semibold text-2xl">Welcome to Colauncha Dashboard</h1>
+        <h1 className="font-semibold text-2xl">Welcome to Colauncha</h1>
+        <p>Colaucha's Dashboard helps manage your queries</p>
         <div className="flex flex-col lg:flex-row gap-20">
           <aside className="flex flex-col items-center justify-center gap-4">
             <Button className="bg-red-400 hover:bg-red-500">logout</Button>
-            <img src={placehold} alt="" className="w-40 h-40 rounded-full" />
+            <img src={placehold} alt="" className="w-40 h-40 rounded-full border" />
           </aside>
           <main className="flex flex-col gap-10">
             <div className="flex gap-20 items-center justify-center">

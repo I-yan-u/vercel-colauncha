8a9b9c08cec10cd901b31bedd72f149aae623e37
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-11-14 12:10:42 +0100

    set up auth and protected route and added a dashboard section for making request

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -4,8 +4,8 @@ import { placehold } from "../constants";
 
 const Dashboard = () => {
   return (
-    <div className="h-screen w-full">
-      <div className="w-full mx-auto flex flex-col mt-28 gap-10 items-center justify-center">
+    <div className="min-h-screen w-full">
+      <div className="w-[75%] mx-auto flex flex-col mt-28 gap-10 items-center justify-center ">
         <h1 className="font-semibold text-2xl">Welcome to Colauncha</h1>
         <p>Colaucha's Dashboard helps manage your queries</p>
         <div className="flex flex-col lg:flex-row gap-20">
@@ -13,17 +13,19 @@ const Dashboard = () => {
             <Button className="bg-red-400 hover:bg-red-500">logout</Button>
             <img src={placehold} alt="" className="w-40 h-40 rounded-full border" />
           </aside>
-          <main className="flex flex-col gap-10">
-            <div className="flex gap-20 items-center justify-center">
+          <main className="flex flex-col gap-10  border p-4 rounded-md">
+            <div className="flex gap-20 items-start justify-center">
               <div className="">
                 <Button className="bg-blue-700 hover:bg-blue-400">
                   <Link to="/talent-request">Request As a Talent</Link>
                 </Button>
+                <p className="w-36">Talents can join Colauncha Team via this route</p>
               </div>
               <div className="">
                 <Button className="bg-green-600 hover:bg-green-300">
-                  <Link to="/project-request">Request As a Talent</Link>
+                  <Link to="/project-request">Request for Talents</Link>
                 </Button>
+                <p className="w-36">Project owners and founders can requests for talents and as well asks for Colauncha's team to Build their project(s)</p>
               </div>
             </div>
           </main>
